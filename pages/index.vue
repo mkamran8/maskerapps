c38629d360b44892e63f483941176cbe45f68b46
mkamran8 <muhammad.kamran858@gmail.com>
2022-05-10 20:09:33 +0500

    labels centered on the home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,28 +4,28 @@
             <div class="felx flex-col">
               <img class="my-4"
                src="~/assets/hero_bg.svg" alt="We do development">
-               <h2 class="flex justify-center text-2xl text-red mb-4">Custom Software Solutions Company</h2>
+               <h2 class="flex justify-center text-2xl text-red mb-4 text-center">Custom Software Solutions Company</h2>
             </div>
         </div>
         <div class="w-full py-4 b">
             <div class="max-w-screen-lg items-center mx-auto">
-                <h2 class="text-slate-800 text-lg font-bold ">Services</h2>
+                <h2 class="text-slate-800 text-lg font-bold my-2 ">Services</h2>
         
                 <div class="flex justify-between">
                     <div class="w-60 bg-white border-b-2 border-red">
                         <img class="h-72"
                         src="~/assets/software-dev.svg" alt="Software Development">
-                        <h3 class="flex justify-center font-bold text-slate-800 bg-white px-2 py-2">Software Development</h3>
+                        <h3 class="flex justify-center font-bold text-slate-800 bg-white px-2 py-2 text-center">Software Development</h3>
                     </div>
                     <div class="w-60 bg-white border-b-2 border-red">
                         <img class="h-72"
                         src="~/assets/mobile-dev.svg" alt="Software Development">
-                        <h3 class="flex justify-center font-bold text-slate-800 bg-white px-2 py-2">Mobile Development</h3>
+                        <h3 class="flex justify-center font-bold text-slate-800 bg-white px-2 py-2 text-center">Android & iOS Consulting</h3>
                     </div>
                     <div class="w-60 bg-white border-b-2 border-red">
                         <img class="h-72"
                         src="~/assets/website-dev.svg" alt="Software Development">
-                        <h3 class="flex justify-center font-bold text-slate-800 bg-white px-2 py-2">Website Development</h3>
+                        <h3 class="flex justify-center font-bold text-slate-800 bg-white px-2 py-2 text-center">Website Development</h3>
                     </div>
                 </div>
             </div>
