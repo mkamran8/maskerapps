0b0233cc2c99b986229138a2ee09c64f174e9a3c
mkamran8 <muhammad.kamran858@gmail.com>
2022-05-10 15:42:56 +0500

    tailwindcss package installed.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily : {
+        sans : ['Poppins', 'sans-serif']
+      },
+      colors : {
+        red : '#F50057'
+      }
+    },
+  },
+  plugins: [],
+}
